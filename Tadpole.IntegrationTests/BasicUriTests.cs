16ac03212447bfed2eea1bcd99972d35e4707af1
Philip Bathe <philip.bathe@investmentsoftwareltd.com>
2020-08-26 13:59:47 +0100

    Initial web and integration test framework

---
+++ Tadpole.IntegrationTests/BasicUriTests.cs
@@ -0,0 +1,32 @@
+using NUnit.Framework;
+using Shouldly;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Tadpole.IntegrationTests
+{
+    [TestFixture]
+    public class BasicUriTests
+    {
+        private HttpClient _client;
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            var _factory = new IntegrationTestWebApplicationFactory<Tadpole.Web.Startup>();
+            _client = _factory.CreateClient();
+        }
+
+        [Test]
+        public async Task HomepageShouldReturnSuccessAndCorrectContentType()
+        {
+            // Act
+            var response = await _client.GetAsync("/");
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+            response.Content.Headers.ContentType.ToString().ShouldBe("text/html; charset=utf-8");
+        }
+    }
+}
---
+++ Tadpole.IntegrationTests/IntegrationTestWebApplicationFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Tadpole.IntegrationTests
+{
+    public class IntegrationTestWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup: class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var sp = services.BuildServiceProvider();
+            });
+        }
+    }
+}
